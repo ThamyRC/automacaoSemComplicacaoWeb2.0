eb19b6188153a54373c974778394f35d5664224d
Thamy <thamyrcardoso@gmail.com>
2022-06-06 21:15:41 -0300

    aula 20 massa de dados

--- src/test/java/br/com/chronosAcademy/steps/LoginSteps.java
+++ src/test/java/br/com/chronosAcademy/steps/LoginSteps.java
@@ -15,6 +15,7 @@ import java.util.Map;
 
 public class LoginSteps {
     LoginPage loginPage;
+    String username;
 
     @Before
     public void iniciaNavegador() {
@@ -67,7 +68,7 @@ public class LoginSteps {
 
     @Quando("os campos de login forem preenchidos da seguinte forma")
     public void osCamposDeLoginForemPreenchidosDaSeguinteForma(Map<String, String> map) {
-        String username = map.get("login");
+        username = map.get("login");
         String password = map.get("password");
         boolean remember = Boolean.parseBoolean(map.get("remember"));
 
@@ -83,10 +84,12 @@ public class LoginSteps {
 
     @Entao("deve ser possivel logar no sistema")
     public void deveSerPossivelLogarNoSistema() {
+        Assert.assertEquals(username, loginPage.getUsusarioLogado());
     }
 
     @Entao("o sistema deve exibir uma mensagem de erro")
     public void oSistemaDeveExibirUmaMensagemDeErro() {
+        Assert.assertEquals("Incorrect user name or password.", loginPage.getErroLogin());
     }
 
     @Entao("o botao sing in deve permanecer desabilitado")
